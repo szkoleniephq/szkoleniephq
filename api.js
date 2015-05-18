f190aa21105cb479242bbc5f10f875b07ebbfd5d
pkowalewski <kowal@Przemeks-MacBook-Pro.local>
2015-05-18 13:00:51 +0200

    zmiany w api

---
+++ api.js
@@ -0,0 +1,17 @@
+var api = {
+
+	loadConstituencies: function(callback) {
+
+		callback()
+	},
+
+	loadCandidates: function(callback) {
+
+	},
+
+	saveConstituenceResult: function(result, callback) {
+
+	}
+}
+
+exports = api;
