4f3d5f8a2f234f559175376231207aeaca98891b
pkowalewski <kowal@Przemeks-MacBook-Pro.local>
2015-05-18 16:45:32 +0200

    events

--- socketRoutes.js
+++ socketRoutes.js
@@ -1,12 +1,27 @@
 module.exports = function(io, db, events) {
 
+	var getVotes = function(callback) {
+		db.getCandidates().then(function(candidates) {
+			candidates.forEach
+		})
+	}
 
 	io.on('connection', function(socket) {
 		console.log('client connected')
 	});
 
+	events.on('candidatesLoaded', function() {
+
+		db.getCandidates().then(function(candidates) {
+			console.log('result-sync', candidates);
+			io.sockets.emit('result-sync', candidates);
+		})
+	});
 
 	events.on('newVote', function() {
-		io.emit('votes', "sample vote");
+		db.getCandidates().then(function(candidates) {
+			console.log('result-sync', candidates);
+			io.sockets.emit('result-sync', candidates);
+		})
 	})
 };
