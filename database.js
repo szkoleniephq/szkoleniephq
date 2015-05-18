4bf7842393aeeac9aa143f52b99db984f630ad72
szkoleniephq <kowal@Przemeks-MacBook-Pro.local>
2015-05-18 17:52:35 +0200

    database

--- database.js
+++ database.js
@@ -1,97 +1,97 @@
 var Db = require('mongodb').Db,
-    MongoClient = require('mongodb').MongoClient,
-    Server = require('mongodb').Server,
-    ReplSetServers = require('mongodb').ReplSetServers,
-    ObjectID = require('mongodb').ObjectID,
-    q = require('q'),
-    db = null,
-    db_promise = q.defer();
+	MongoClient = require('mongodb').MongoClient,
+	Server = require('mongodb').Server,
+	ReplSetServers = require('mongodb').ReplSetServers,
+	ObjectID = require('mongodb').ObjectID,
+	q = require('q'),
+	db = null,
+	db_promise = q.defer();
 
 if (!db) {
-    MongoClient.connect('mongodb://user:user@ds053139.mongolab.com:53139/db1', function(err, _db) {
-        if (err) {
-            console.log('error', err);
-            db_promise.reject();
-            return;
-        }
-        db = _db;
-        db_promise.resolve(db);
-    });
+	MongoClient.connect('mongodb://user:user@ds053139.mongolab.com:53139/db1', function(err, _db) {
+		if (err) {
+			console.log('error', err);
+			db_promise.reject();
+			return;
+		}
+		db = _db;
+		db_promise.resolve(db);
+	});
 } else {
-    db_promise.resolve(db);
+	db_promise.resolve(db);
 }
 
 module.exports = {
-    getConstituencies: function() {
-        var defered = q.defer();
-        db_promise.promise.then(function(db) {
-            db.collection('constituencies').find({}).toArray(function(err, data) {
-                if (err) {
-                    console.log('error', err);
-                    defered.reject();
-                    return;
-                }
-                defered.resolve(data);
-            });
-        });
-        return defered.promise;
-    },
+	getConstituencies: function() {
+		var defered = q.defer();
+		db_promise.promise.then(function(db) {
+			db.collection('constituencies').find({}).toArray(function(err, data) {
+				if (err) {
+					console.log('error', err);
+					defered.reject();
+					return;
+				}
+				defered.resolve(data);
+			});
+		});
+		return defered.promise;
+	},
 
-    getCandidates: function() {
-        var defered = q.defer();
-        db_promise.promise.then(function(db) {
-            db.collection('candidates').find({}, {
-                name: true,
-                result: true,
-            }).toArray(function(err, data) {
-                if (err) {
-                    console.log('error', err);
-                    defered.reject();
-                    return;
-                }
-                defered.resolve(data);
-            });
-        });
-        return defered.promise;
-    },
+	getCandidates: function() {
+		var defered = q.defer();
+		db_promise.promise.then(function(db) {
+			db.collection('candidates').find({}, {
+				name: true,
+				result: true,
+			}).toArray(function(err, data) {
+				if (err) {
+					console.log('error', err);
+					defered.reject();
+					return;
+				}
+				defered.resolve(data);
+			});
+		});
+		return defered.promise;
+	},
 
-    storeVotes: function(votes) {
-        var defered = q.defer();
-        db_promise.promise.then(function(db) {
-            for (var _idx = 0; _idx < votes.length; _idx++) {
-                var inx = _idx;
-                var vote = votes[idx];
-                console.log("Vote: ", vote);
-                db.collection('constituencies').find({
-                    _id: ObjectID(vote.constituencyId)
-                }).count(function(err, count) {
-                    console.log("Count: ", count);
-                    if (err || !count) {
-                        defered.resolve();
-                        return;
-                    }
-                    db.collection('candidates').update({
-                        _id: ObjectID(vote.candidateId)
-                    }, {
-                        $push: {
-                            votes: {
-                                constituencyId: vote.constituencyId,
-                                votes: vote.votes
-                            }
-                        },
-                        $inc: {
-                            result: vote.votes
-                        }
-                    });
-                });
-            }
-            defered.resolve();
-            console.log("OK");
-        });
-        return defered.promise;
-    },
+	storeVotes: function(votes) {
+		var defered = q.defer();
+		db_promise.promise.then(function(db) {
+			for (var _idx = 0; _idx < votes.length; _idx++) {
+				var idx = _idx;
+				var vote = votes[idx];
+				console.log("Vote: ", vote);
+				db.collection('constituencies').find({
+					_id: ObjectID(vote.constituencyId)
+				}).count(function(err, count) {
+					console.log("Count: ", count);
+					if (err || !count) {
+						defered.resolve();
+						return;
+					}
+					db.collection('candidates').update({
+						_id: ObjectID(vote.candidateId)
+					}, {
+						$push: {
+							votes: {
+								constituencyId: vote.constituencyId,
+								votes: vote.votes
+							}
+						},
+						$inc: {
+							result: vote.votes
+						}
+					});
+				});
+			}
+			defered.resolve();
+			console.log("OK");
+		});
+		return defered.promise;
+	},
 
-    getDb: function() {
-        return db_promise.promise;
-    }
+	getDb: function() {
+		return db_promise.promise;
+	}
 };
