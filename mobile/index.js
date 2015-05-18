81d9812e2d76a3df10dcdcda43a90593b222deda
Piotr Cichosz <piotr@fronthero.pl>
2015-05-18 16:01:01 +0200

    added getting rela data

--- mobile/index.js
+++ mobile/index.js
@@ -1,35 +1,10 @@
-var candidates = new CandidateCollection([
-	{
-		'id': 1,
-		'name': 'Komor'
-	},
-	{
-		'id': 2,
-		'name': 'Dupa'
-	},
-	{
-		'id': 3,
-		'name': 'Chuck'
-	}
-]);
-
-var constituencies = new ConstituencyCollection([
-	{
-		'id': 1,
-		'name': 'Kozia Wólka'
-	},
-	{
-		'id': 2,
-		'name': 'Kozia Dupa'
-	}
-]);
-
-
+var constituencies = new ConstituencyCollection();
 var mainView = new MobileMainView({
 
 	collection: constituencies,
+
 	el: $('#webapp-container')
 
 });
 
-mainView.render();
+constituencies.fetch();
